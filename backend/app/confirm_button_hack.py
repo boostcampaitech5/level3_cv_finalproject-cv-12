5b96f9e74002fe0a20c1084784dae2a30e4e6273
Hyunmin-H <handy389@naver.com>
2023-07-16 14:34:31 +0000

    [Feat] frontend 구성 초안 #8
    - 3개의 column으로 나눔(target, garment, result)
    - 임시로 authenticate 기능 삭제

--- backend/app/confirm_button_hack.py
+++ backend/app/confirm_button_hack.py
@@ -51,7 +51,7 @@ def cache_on_button_press(label, **cache_kwargs):
                 if st.button(label):
                     cache_entry.evaluate()
                 else:
-                    raise st.script_runner.StopException
+                    st.stop()
             return cache_entry.return_value
 
         return wrapped_func
