90420311183f3877fa6942dce7bcef9de25afff1
Hyunmin-H <handy389@naver.com>
2023-07-24 06:24:47 +0000

    [Design] 웹 guideline 구도 변경 #25
    - guideline 위치를 위로 변경
    - Human에 예시 사진 추가
    
    related to: #8

--- backend/app/frontend.py
+++ backend/app/frontend.py
@@ -29,7 +29,7 @@ def apply_custom_font(text, font_size=48):
         with open(font_path, "rb") as f:
             font_data = f.read()
         font_base64 = base64.b64encode(font_data).decode("utf-8")
-        font_style = f"font-family: 'CustomFont' ; font-size: {font_size}px;text-align: center;"
+        font_style = f"font-family: 'CustomFont' ; font-size: {font_size}px;"
         styled_text = f'<div style="{font_style}";>{text}</div>'
         return f'<style>@font-face {{font-family: "CustomFont"; src: url(data:font/ttf;base64,{font_base64})}}</style>{styled_text}'
     except Exception as e:
@@ -38,45 +38,44 @@ def apply_custom_font(text, font_size=48):
 
 def user_guideline_for_human():
 
-    text1 = '전신 사진을 넣어주세요. '
-    text2 = '최대한 머리와 발끝을'
-    text3 = '사진의 위아래 테두리에 맞게 찍어주세요!'
-    styled_text1 = apply_custom_font(text1, 36)
-    styled_text2 = apply_custom_font(text2, 24)
-    styled_text3 = apply_custom_font(text3, 24)
-    st.write(' ')
-    st.write(' ')
-    st.write(' ')
-    st.write(' ')
-    st.markdown(styled_text1, unsafe_allow_html=True)
-    st.write(' ')
-    st.markdown(styled_text2, unsafe_allow_html=True)
-    st.markdown(styled_text3, unsafe_allow_html=True)
-
+    # text1 = '전신 사진을 넣어주세요.'
+    # text2 = '아래 예시 사진처럼, 머리와 발끝을'
+    # text3 = '최대한 사진의 위아래 테두리에 맞게 찍어주세요.'
+    # styled_text1 = apply_custom_font(text1, 24)
+    # styled_text2 = apply_custom_font(text2, 24)
+    # styled_text3 = apply_custom_font(text3, 24)
     # st.write(' ')
-    # text1 = """<h4 style='text-align: center;'> 전신 사진을 넣어주세요.  </h4>"""
-    # text1 = """<h4 style='text-align: center;'> 최대한 머리와 발끝을  </h4>"""
-    # text2 = """<h4 style='text-align: center;'> 사진의 위아래 테두리에 맞게 찍어주세요! </h4>"""
+    # st.write(' ')
+    # st.write(' ')
+    # st.write(' ')
+    # st.markdown(styled_text1, unsafe_allow_html=True)
+    # st.write(' ')
+    # st.markdown(styled_text2, unsafe_allow_html=True)
+    # st.markdown(styled_text3, unsafe_allow_html=True)
+
+    st.write(' ')
+    text1 = """<h5 style=''> 1. 전신 사진을 넣어주세요.  </h5>"""
+    text2 = """<h5 style=''> 2. 아래 예시 사진과 같이, 최대한 사진의 위아래 테두리에 맞게 찍어주세요. </h5>"""
     
-    # st.markdown(text1, unsafe_allow_html=True)
-    # st.markdown(text2, unsafe_allow_html=True)
+    st.markdown(text1, unsafe_allow_html=True)
+    st.markdown(text2, unsafe_allow_html=True)
 
 def user_guideline_for_garment():
-    text1 = '상의, 하의, 상의&하의, 드레스 카테고리를 선택 후,'
-    text2 = '단일 옷 사진을 넣어주세요 !'
-    styled_text1 = apply_custom_font(text1, 24)
-    styled_text2 = apply_custom_font(text2, 36)
-    st.write(' ')
-    st.write(' ')
-    st.markdown(styled_text1, unsafe_allow_html=True)
-    st.markdown(styled_text2, unsafe_allow_html=True)
-
+    # text1 = '1. 상의, 하의, 상의&하의, 드레스 카테고리를 선택해주세요.'
+    # text2 = '2. 단일 옷 사진을 넣어주세요.'
+    # styled_text1 = apply_custom_font(text1, 24)
+    # styled_text2 = apply_custom_font(text2, 24)
     # st.write(' ')
-    # text1 = """<h4 style='text-align: center;'> 상의, 하의, 드레스 카테고리를 선택 후, </h4>"""
-    # text2 = """<h4 style='text-align: center;'> 단일 옷 사진을 넣어주세요 !  </h4>"""
+    # st.write(' ')
+    # st.markdown(styled_text1, unsafe_allow_html=True)
+    # st.markdown(styled_text2, unsafe_allow_html=True)
+
+    st.write(' ')
+    text1 = """<h5 style=''> 1. 상의, 하의, 상의&하의, 드레스 카테고리를 선택해주세요. </h5>""" #text-align: center;
+    text2 = """<h5 style=''> 2. 단일 옷 사진을 넣어주세요. </h5>"""
     
-    # st.markdown(text1, unsafe_allow_html=True)
-    # st.markdown(text2, unsafe_allow_html=True)
+    st.markdown(text1, unsafe_allow_html=True)
+    st.markdown(text2, unsafe_allow_html=True)
 
 def check_modelLoading():
     api_url = "http://localhost:8001/get_boolean"
@@ -98,7 +97,7 @@ def main():
         
         with col1:
             st.header("Human")
-            
+            user_guideline_for_human()
             # target_img = Image.open('/opt/ml/user_db/input/buffer/target/target.jpg')
             uploaded_target = st.file_uploader("Choose an target image", type=["jpg", "jpeg", "png"])
 
@@ -108,11 +107,12 @@ def main():
 
                 st.image(target_img, caption='Uploaded target Image')
             else : 
-                user_guideline_for_human()
+                example_img = Image.open('/opt/ml/level3_cv_finalproject-cv-12/backend/app/utils/example.jpg')
+                st.image(example_img, caption='Example of target Image')
         
         with col2:
             st.header("Cloth")
-
+            user_guideline_for_garment()
             category_list = ['Upper', 'Lower', 'Upper & Lower', 'Dress']
             selected_category = st.selectbox('Choose an category of garment', category_list)
             # uploaded_garment = Image.open('/opt/ml/user_db/input/buffer/garment/garment.jpg')
@@ -123,9 +123,8 @@ def main():
             if selected_category == 'Upper & Lower':
                 uploaded_garment1 = st.file_uploader("Choose an upper image", type=["jpg", "jpeg", "png"])
                 uploaded_garment2 = st.file_uploader("Choose an lower image", type=["jpg", "jpeg", "png"])
-                print(uploaded_garment1, uploaded_garment2)
-                if not uploaded_garment1 and not uploaded_garment2 : 
-                    user_guideline_for_garment()
+                # if not uploaded_garment1 and not uploaded_garment2 : 
+                #     user_guideline_for_garment()
 
                 col2_1, col2_2, = st.columns([1,1])
                 with col2_1:
@@ -156,8 +155,8 @@ def main():
             else : 
                 uploaded_garment = st.file_uploader("Choose an garment image", type=["jpg", "jpeg", "png"])
 
-                if not uploaded_garment : 
-                    user_guideline_for_garment()
+                # if not uploaded_garment : 
+                #     user_guideline_for_garment()
 
                 if uploaded_garment:
                     garment_bytes = uploaded_garment.getvalue()
