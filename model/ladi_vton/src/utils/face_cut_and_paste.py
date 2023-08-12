53da68d553447861b22de8519b3f99268cb97170
김도환_T5024 <121927513+rlaehghks5@users.noreply.github.com>
2023-08-12 12:44:08 +0900

    Update face_cut_and_paste.py

--- model/ladi_vton/src/utils/face_cut_and_paste.py
+++ model/ladi_vton/src/utils/face_cut_and_paste.py
@@ -25,7 +25,6 @@ label_map={
 from PIL import Image, ImageDraw, ImageOps
 import PIL
 import os
-# /opt/ml/user_db/schp/buffer/target.png
 import numpy as np
 
 import matplotlib.pyplot as plt
@@ -40,13 +39,11 @@ def main_cut_and_paste(category, db_dir, target_name='target.jpg'):
     parse_name = im_name.replace('.jpg', '.png')
 
 
-    # /opt/ml/user_db/input/buffer/target/target.jpg
 
     origin_image = Image.open(os.path.join(dataroot, 'input/buffer/target', im_name))
     origin_image = origin_image.resize((384,512))
     origin_np = np.array(origin_image)
 
-    # /opt/ml/user_db/ladi/buffer/lower_body.png
 
     generatived_image = Image.open(os.path.join(dataroot, 'ladi/buffer', f'{category}.png'))
     generatived_np = np.array(generatived_image)
@@ -56,7 +53,6 @@ def main_cut_and_paste(category, db_dir, target_name='target.jpg'):
     im_parse = im_parse.resize((384, 512), Image.NEAREST)
     parse_array = np.array(im_parse)
 
-    # if category == 'face':
     parser_mask_changeable = (parse_array == label_map["hair"]).astype(np.float32) + \
                         (parse_array == label_map["head"]).astype(np.float32) + \
                         (parse_array == label_map["sunglasses"]).astype(np.float32) + \
@@ -65,31 +61,12 @@ def main_cut_and_paste(category, db_dir, target_name='target.jpg'):
                         (parse_array == label_map["scarf"]).astype(np.float32)
                             
     indices = np.where(parser_mask_changeable == 1)
-    # print(len(indices[0]))
-    # if category == 'lower_body':
-    #     parser_mask_changeable = (parse_array == label_map["skirt"]).astype(np.float32) + \
-    #                             (parse_array == label_map["pants"]).astype(np.float32)
-    #                             # (parse_array == label_map["left_leg"]).astype(np.float32) + \
-    #                             # (parse_array == label_map["right_leg"]).astype(np.float32) + \
-    #                             # (parse_array == label_map["right_shoe"]).astype(np.float32) + \
-    #                             # (parse_array == label_map["left_shoe"]).astype(np.float32)
-                                
-                                
-    #     indices = np.where(parser_mask_changeable == 0)
-    # elif category == 'upper_body':
-    #     parser_mask_changeable = (parse_array == label_map["upper_clothes"]).astype(np.float32)
-    #                             # (parse_array == label_map["left_arm"]).astype(np.float32) + \
-    #                             # (parse_array == label_map["right_arm"]).astype(np.float32)
-    #     indices = np.where(parser_mask_changeable == 0)
-    # elif category == 'dresses':
-    #     parser_mask_changeable = (parse_array == label_map["dress"]).astype(np.float32)
-    #     indices = np.where(parser_mask_changeable == 0)
+
 
     def overlay_arrays(origin_np, generatived_np, indices):
-        # indices에서 위치 정보를 추출합니다.
+
         overlay_indices = (indices[0], indices[1])
         
-        # generatived_np를 origin_np 해당 위치에 덮어씌웁니다.
         generatived_np[overlay_indices] = origin_np[overlay_indices]
         
         return generatived_np
@@ -97,9 +74,6 @@ def main_cut_and_paste(category, db_dir, target_name='target.jpg'):
     result_array = overlay_arrays(origin_np, generatived_np, indices)
     result_array = Image.fromarray(result_array)
     
-    # file = os.path.join(dataroot, 'ladi/buffer', f'{category}.png')
-    # if os.path.isfile(file):
-    #     print('파일있냐')
-    #     os.remove(file)
+
     
     result_array.save(os.path.join(dataroot, 'ladi/buffer', f'{category}.png'))
